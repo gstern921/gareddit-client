800b0d652880bc7ac2f5e244b957a659fa068828
Gary Stern <gcrunkster@gmail.com>
2020-12-06 18:05:45 -0500

    update config urls

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
 const NODE_ENV = process.env.NODE_ENV || "development";
 export const IS_PROD = NODE_ENV !== "development";
-export const GRAPHQL_URL = "http://localhost:4000/graphql";
-export const NEXT_PUBLIC_API_URL = "http://localhost:4000/graphql";
+export const GRAPHQL_URL = "https://gareddit-server.herokuapp.com/graphql";
+export const NEXT_PUBLIC_API_URL = "https://gareddit-server.herokuapp.com";
